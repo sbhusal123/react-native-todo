33285ab08d679c1413ecbc0b46784204b62fa901
sbhusal123 <suryabhusal11@gmail.com>
2021-04-23 01:09:02 +0545

    Icons and flex width adjust on text.

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -1,21 +1,31 @@
 import React from "react";
-import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+import { MaterialIcons } from "@expo/vector-icons";
 
 export default function TodoItem({ item, pressHandler }) {
     return (
         <TouchableOpacity onPress={() => pressHandler(item.key)}>
-            <Text style={styles.item}>{item.text}</Text>
+            <View style={styles.item}>
+                <MaterialIcons name="delete" size={18} color="#333" />
+                <Text style={styles.text}>{item.text}</Text>
+            </View>
         </TouchableOpacity>
     );
 }
 
 const styles = StyleSheet.create({
     item: {
+        flex: 1,
+        flexDirection: "row",
         padding: 16,
         marginTop: 16,
         borderColor: "#bbb",
         borderWidth: 1,
         borderStyle: "dashed",
         borderRadius: 10
+    },
+    text: {
+        marginLeft: 10
     }
 });
